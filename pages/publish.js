308b0fd2b6ce73d68add5079373af4aaaf51b6b6
Paul Shorey <ps@artspaces.net>
2021-03-28 23:52:41 -0500

    /publish

---
+++ pages/publish.js
@@ -0,0 +1,15 @@
+export default function () {
+  return (
+    <div>
+      <iframe
+        src="https://api.vercel.com/v1/integrations/deploy/prj_pzUKqR8BsL2iubEuBcgRj7kbSGfi/uoOttSL017"
+        width="100%"
+      />
+      <h1>Your website is now being published.</h1>
+      <p>
+        <b>Please wait 2 minutes.</b> You will receive an email when the deployment is completed, and your website
+        changes are ready to view.
+      </p>
+    </div>
+  );
+}
