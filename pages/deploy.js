5a0e5a5be3e1178a0231ac36590b380b0c7682d1
Paul Shorey <ps@artspaces.net>
2021-03-28 23:30:10 -0500

    deploy url

---
+++ pages/deploy.js
@@ -0,0 +1,9 @@
+export default function() {
+  return (
+    <iframe
+      src="https://api.vercel.com/v1/integrations/deploy/prj_pzUKqR8BsL2iubEuBcgRj7kbSGfi/uoOttSL017"
+      width="100%"
+      height="100%"
+    />
+  );
+}
